8522a73f9e4bd70909ab2cd331cf28f9fa4f682a
utsav <utsav@rtheta.in>
2020-02-08 18:31:28 +0530

    outline for replication_stratergy base class and initialised a package

---
+++ replication_stratergy.py
@@ -0,0 +1,7 @@
+class replication_stratergy:
+    """base class for replication stratergy"""
+
+    def __init__(self):
+        self.mechanism = "active/passive/semi-active"   #placeholder for actual mechanism implementaion object
+        self.num_of_replica = 0
+        self.backup_config = [] #a list of configurations(YAML file) for backup VMs
