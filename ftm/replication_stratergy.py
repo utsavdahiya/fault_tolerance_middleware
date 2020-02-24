9979f56093686a547af4efe9b3dcdc2c7d5bb7b4
utsav <utsav@rtheta.in>
2020-02-24 15:25:37 +0530

    replication strat

--- ftm/replication_stratergy.py
+++ ftm/replication_stratergy.py
@@ -2,10 +2,13 @@
     defines the functions required to implement a replication stratergy
 '''
 
-class replication_stratergy:
+class Replication_stratergy:
     """base class for replication stratergy"""
 
     def __init__(self):
         self.mechanism = "active/passive/semi-active"   #placeholder for actual mechanism implementaion object
         self.num_of_replica = 0
         self.backup_config = [] #a list of configurations(YAML file) for backup VMs
+    
+    def replicationStrat(self):
+        raise NotImplementedError()
