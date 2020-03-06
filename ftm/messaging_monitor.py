672a82bffd69908251a5d8f0099bebe21236630e
utsav <utsav@rtheta.in>
2020-03-06 14:01:52 +0530

    temp commit

--- ftm/messaging_monitor.py
+++ ftm/messaging_monitor.py
@@ -1,5 +1,18 @@
 '''
+module that handles all communicaiton
+
+its purpose is to route and buffer messages
 it performs two functions:
     >an interface for communication bw all the composition
     >it also acts as interface bw the ftm and cloudsim
 '''
+
+class MessagingMonitor():
+
+    def __init__(self):
+        pass
+
+    def connect(self, destination):
+        pass
+
+    def disconnect(self, )
