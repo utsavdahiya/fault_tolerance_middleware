87f970b80fe68909d9b099eb23077cead563612b
Uday <uday.vig@gmail.com>
2020-03-13 19:43:16 +0530

    WebScoket failed attempt

---
+++ WebSocketImpl/src/main/java/Client.java
@@ -0,0 +1,23 @@
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        WebSocketClient client = new StandardWebSocketClient();
+
+        WebSocketStompClient stompClient = new WebSocketStompClient(client);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        StompSessionHandler sessionHandler = new MyStompSessionHandler();
+        stompClient.connect("ws://192.168.1.8:8081/ws", sessionHandler);
+
+        //System.out.println("yolo");
+
+        new Scanner(System.in).nextLine();
+    }
+}
