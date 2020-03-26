f795eccd6728da6d4ea3907742dddbb337e7a14f
utsav <utsav@rtheta.in>
2020-03-27 00:30:15 +0530

    replica invoker and VM class

--- ftm/replication_mgr/replica_invoker.py
+++ ftm/replication_mgr/replica_invoker.py
@@ -1,10 +1,14 @@
 '''to invoke the required number of replicas(via the resource manager) and return a list of them
     it receives the requirements from composition engine and returns the list to it
 '''
+from .VM import VM
 
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+def get_id():
+    pass
 
 class invoker:
     '''class to invole the replicas'''
@@ -13,4 +17,31 @@ class invoker:
         self.default_config = {}    #a json of config
 
     def add_config(self, configs):
-        pass
+        '''adds a new configuration to the invoker
+        
+        Args:
+            configs: A list of dictionaries containing replication config:
+                    example: [{config_name: {config_json}}, {}, ...]
+        '''
+        for item in configs:
+            if item not in self.configs:
+                for name, config in item:
+                    self.configs[name] = config     #add config to self.configs
+
+    def instantiate_replicas(self, config_info):
+        '''asks resource manager to invoke specified configs
+        
+        Args:
+            config_info: A list of tuples of config names(IDs) to be invoked(instantiated):
+                example: [(config_name, number of VMs to be invoked), (), ...]
+        '''
+        VMs = dict()
+        for item in config_info:
+            vm_list = []
+            for i in range(item[2]):
+                id = get_id()
+                config_name = item[0]
+                config = self.configs.get(config_name)
+                #create a new VM object
+                vm = VM(id, config)
+                
