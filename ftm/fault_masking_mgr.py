08103df68a4545a52bfacaac1ddc888021c24485
utsav <utsav.163dahiya@gmail.com>
2020-04-21 16:07:13 +0530

    demo successful

--- ftm/fault_masking_mgr.py
+++ ftm/fault_masking_mgr.py
@@ -15,10 +15,13 @@ class FaultMasking:
     async def handle_fault(self, data):
         ftm = data['ftm']
         vm_id = data['vm_id']
+        #change vm status to inactive
+        ftm.all_VMs[vm_id].status = 'inactive'
         #migrating the vm
         msg = {
                 "desc": "migration",
                 "id": vm_id
             }
+        logger.info(colored(f"migrating faulty VM [{vm_id}]", 'red'))
         await ftm.msg_monitor.send_json(msg, 'cloud')
-        logger.info(colored(f"vm id[{vm_id}] has been successfully migrated to another host"))
+        logger.info(colored(f"vm id[{vm_id}] has been successfully migrated to another host", 'green', 'on_white'))
