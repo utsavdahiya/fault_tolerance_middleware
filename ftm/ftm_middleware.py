b760cfbc1048389367b989ed36e361d8feda1cf5
utsav <utsav.163dahiya@gmail.com>
2020-05-03 20:45:04 +0530

    failures dict error

--- ftm/ftm_middleware.py
+++ ftm/ftm_middleware.py
@@ -130,12 +130,16 @@ class FTM:
         total_duration = 0
         table = PrettyTable()
         table.field_names = ["VM ID", "Failure Duration"]
-        for primary_vm, stat in failures:
-            #primary_vm= primary_vm_d, stat= list of failures
-            for failure in stat[1:]:
-                duration = stat.end - stat.start
-                table.add_row([primary_vm, duration])
-                total_duration += duration
+        try:
+            for primary_vm, stat in failures:
+                #primary_vm= primary_vm_d, stat= list of failures
+                for failure in stat[1:]:
+                    duration = stat.end - stat.start
+                    table.add_row([primary_vm, duration])
+                    total_duration += duration
+        except Exception as e:
+            logger.error(colored(f"failure is of type: {type(failures)}"))
+            logger.error(colored(f"error: {e}"))
         logger.info(table)
         logger.info(colored(f"Total Failure Duration:\t {total_duration}", "green"))
 
