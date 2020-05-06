f62cc9da2b7c03db0c0b1b3dc2bd9529fe293e3a
utsav <utsav.163dahiya@gmail.com>
2020-05-06 21:34:12 +0530

    automation and graph display

--- results/output.py
+++ results/output.py
@@ -7,7 +7,7 @@ def print_duration(fault_rate, List):
     Args:
         fault_rate
     '''
-    y = np.arange(0.5, 1.0, 0.1)
+    y = np.arange(1, len(List)+1, 1)
     fig, ax = plt.subplots()
     ax.plot(y, List)
     ax.set(xlabel='Epochs', ylabel='Availability', title=str(fault_rate))
@@ -16,13 +16,16 @@ def print_duration(fault_rate, List):
     fig.savefig(f"fault_{fault_rate}_.png")
     plt.show()
 
-file_name = "default_ouput.pkl"
+def main():
+    file_name = "default_ouput.pkl"
 
-with open(file_name, 'rb') as handle:
-    result = pickle.load(handle)
+    with open(file_name, 'rb') as handle:
+        result = pickle.load(handle)
 
-#printing one simulation durations
-for fault_rate, record in result:
-    #displaying fault rate
-    print("displaying")
-    print_duration(fault_rate, record['duration'])
+    #printing one simulation durations
+    for fault_rate, record in result:
+        #displaying fault rate
+        print("displaying")
+        print_duration(fault_rate, record['duration'])
+
+main()
