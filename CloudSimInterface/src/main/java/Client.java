16ffb1d5678d41622f4ef2d4fb367d211faa89e5
Uday <uday.vig@gmail.com>
2020-05-06 18:04:25 +0530

    Works.

--- CloudSimInterface/src/main/java/Client.java
+++ CloudSimInterface/src/main/java/Client.java
@@ -1,3 +1,4 @@
+import org.apache.commons.math3.distribution.BetaDistribution;
 import org.cloudbus.cloudsim.allocationpolicies.VmAllocationPolicy;
 import org.cloudbus.cloudsim.allocationpolicies.VmAllocationPolicySimple;
 import org.cloudbus.cloudsim.brokers.DatacenterBroker;
@@ -16,6 +17,7 @@ import org.cloudbus.cloudsim.resources.Pe;
 import org.cloudbus.cloudsim.resources.PeSimple;
 import org.cloudbus.cloudsim.schedulers.cloudlet.CloudletSchedulerTimeShared;
 import org.cloudbus.cloudsim.schedulers.vm.VmSchedulerTimeShared;
+import org.cloudbus.cloudsim.util.Conversion;
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModel;
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModelDynamic;
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModelFull;
@@ -59,19 +61,19 @@ public class Client {
     @OnOpen
     public void onOpen(Session session) {
         //System.out.println("yolo7");
-        System.out.println("Connected ... " + session.getId());
+        //System.out.println("Connected ... " + session.getId());
     }
 
     @OnMessage
     public void onMessage(String message, Session session) {
         //System.out.println("yolo8");
-        System.out.println("Received ...." + message);
+        //System.out.println("Received ...." + message);
         messageQueue.add(message);
     }
 
     @OnClose
     public void onClose(Session session, CloseReason closeReason) {
-        System.out.println(String.format("Session %s close because of %s", session.getId(), closeReason));
+        //System.out.println(String.format("Session %s close because of %s", session.getId(), closeReason));
         //latch.countDown();
     }
 
@@ -79,10 +81,10 @@ public class Client {
     public static CountDownLatch latch;
     private static Queue<String> messageQueue = new LinkedList<>();
 
-    private static final int HOST_PES = 2; // Random number as of now, TODO: Get this initially from FTM through a socket connection probably. DONE.
+    private static final int HOST_PES = 10; // Random number as of now, TODO: Get this initially from FTM through a socket connection probably. DONE.
     private static final int SCHEDULING_INTERVAL = 1; // Random number as of now, TODO: Discuss. DONE.
-    private static final int NUMBER_OF_HOSTS = 10; // Random number as of now, TODO: Get this initially from FTM through a socket connection probably. DONE
-    private static final int TIME_TILL_TERMINATION = 100000; // Random time as of now, very big time as discussed
+    private static final int NUMBER_OF_HOSTS = 50; // Random number as of now, TODO: Get this initially from FTM through a socket connection probably. DONE
+    private static final int TIME_TILL_TERMINATION = 40; // Random time as of now, very big time as discussed
     private static final int TOTAL_LOCATIONS = 10; //Random number as of now.
     private static JSONArray locations;
 
@@ -91,8 +93,26 @@ public class Client {
     private static PoissonDistr poisson;
     private static final double MEAN_FAILURE_NUMBER_PER_HOUR = 0;
 
+    private static final double LOWER_BOUND = 0.0;
+    private static final double UPPER_BOUND = 1.0;
+    private static final HashMap<Integer, Pair> locationThreshold = new HashMap<>();
+    private static Set<Map.Entry<Integer, Pair>> locationThresholdEntrySet;
+    private static final double THRESHOLD_PERCENTAGE = 0.5;
+    private static final double THRESHOLD_LOCATION = LOWER_BOUND + ((1 - LOWER_BOUND) * THRESHOLD_PERCENTAGE);
+    private static final double THRESHOLD_HOST = LOWER_BOUND + ((1 - LOWER_BOUND) * THRESHOLD_PERCENTAGE);
+    private static final long SEED_LOCATION = 123456L;
+    private static final long SEED_HOST = 234567L;
+    private static final long SEED_INDEX = 345678L;
+    private static final Random random = new Random(SEED_LOCATION);
+    private static final double ALPHA = 1;
+    private static final double BETA = 1;
+    private static final BetaDistribution betaDistributionLocation = new BetaDistribution(ALPHA, BETA);
+    private static final BetaDistribution betaDistributionHost = new BetaDistribution(ALPHA, BETA);
+    private static final BetaDistribution betaDistributionIndex = new BetaDistribution(ALPHA, BETA);
+
+    private static String finishClientID;
     private static/*final*/ CloudSim simulation;
-    private static /*final*/ Session currSession;
+    private static/*final*/ Session currSession;
     private static/*final*/ DatacenterBroker broker;
     private static/*final*/ Datacenter datacenter;
     static final List<Host> hostList = new ArrayList<>(NUMBER_OF_HOSTS);
@@ -119,20 +139,93 @@ public class Client {
         //Client clientTest = new Client();
         onClockTickListener = eventInfo -> {
             //System.out.println("yolo4");
-            String receivedMessage = getRecMessage();
+
+            /*double randomGeneratedForLocation = LOWER_BOUND + (random.nextDouble() * (UPPER_BOUND - LOWER_BOUND));
+            for(Map.Entry<Integer, Pair> entry : locationThresholdEntrySet){
+                if(randomGeneratedForLocation > entry.getValue().getFirst()
+                        && (int)(eventInfo.getTime()) >= 15){
+                    double randomGeneratedForHost = LOWER_BOUND + (random.nextDouble() * (UPPER_BOUND - LOWER_BOUND));
+                    if(randomGeneratedForHost > entry.getValue().getSecond()){
+                        int factor = NUMBER_OF_HOSTS / TOTAL_LOCATIONS;
+                        int id = entry.getKey();
+                        int startIdx = factor * id;
+                        int randomHostIndex = ThreadLocalRandom.current().nextInt(startIdx, startIdx + factor);
+                        if(hostList.get(randomHostIndex).getFailedPesNumber() != HOST_PES){
+                            fault.generateHostFault(hostList.get(randomHostIndex));
+                        }
+                    }
+                }
+            }*/
+
+            if((int)(eventInfo.getTime()) == TIME_TILL_TERMINATION - 2){
+                JSONObject finalMessage = new JSONObject();
+                finalMessage.put("desc", "finish");
+                finalMessage.put("client_id", finishClientID);
+                try {
+                    currSession.getBasicRemote().sendObject(finalMessage);
+                    System.out.println(finalMessage);
+                } catch (IOException | EncodeException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if((int)(eventInfo.getTime()) >= 15){
+                double randomGeneratedForLocation = LOWER_BOUND + (betaDistributionLocation.sample() * (UPPER_BOUND - LOWER_BOUND));
+                //System.out.println(randomGeneratedForLocation + " loc ");
+                for(Map.Entry<Integer, Pair> entry : locationThresholdEntrySet){
+                    if(randomGeneratedForLocation > entry.getValue().getFirst()){
+                        double randomGeneratedForHost = LOWER_BOUND + (betaDistributionHost.sample() * (UPPER_BOUND - LOWER_BOUND));
+                        //System.out.println(randomGeneratedForHost + " host ");
+                        if(randomGeneratedForHost > entry.getValue().getSecond()){
+                            int factor = NUMBER_OF_HOSTS / TOTAL_LOCATIONS;
+                            int id = entry.getKey();
+                            int startIdx = factor * id;
+                            int randomHostIndex = (int)(Math.floor(startIdx + (betaDistributionIndex.sample() * factor)));
+                            //System.out.println(randomHostIndex + " idx ");
+                            if(hostList.get(randomHostIndex).getFailedPesNumber() != HOST_PES){
+                                fault.generateHostFault(hostList.get(randomHostIndex));
+                            }
+                        }
+                    }
+                }
+            }
+
+            /*if(randomGenerated > THRESHOLD && (int)(eventInfo.getTime()) >= 15){
+                int randomHostIndex = ThreadLocalRandom.current().nextInt(0, hostList.size());
+                fault.generateHostFault(hostList.get(randomHostIndex));
+            }*/
+
+            while(!messageQueue.isEmpty()) {
+                String receivedMessage = getRecMessage();
+                if(receivedMessage.length() != 0){
+                    //System.out.println("yolo9");
+                    JSONObject obj = new JSONObject(receivedMessage);
+                    parseAndRoute(obj);
+                }
+            }
+
+            /*String receivedMessage = getRecMessage();
             if(!receivedMessage.isEmpty()){
                 //System.out.println("yolo9");
                 JSONObject obj = new JSONObject(receivedMessage);
                 parseAndRoute(obj);
-            }
+            }*/
+
+            /*if((int)eventInfo.getTime() == 15){
+                fault.generateHostFault(hostList.get(0));
+                fault.generateHostFault(hostList.get(5));
+                fault.generateHostFault(hostList.get(10));
+            }*/
 
             if((int)eventInfo.getTime() == 15){
-                fault.generateHostFault(hostList.get(0));
+                for(Host host : hostList){
+                    System.out.println(host.getVmList().size());
+                }
             }
 
-            if((int)(simulation.clockInMinutes() * 60) >= TIME_TILL_TERMINATION){
+            /*if((int)(simulation.clockInMinutes() * 60) >= TIME_TILL_TERMINATION){
                 simulation.terminate();
-            }/*else if((int)(simulation.clockInMinutes()) >= 1
+            }else if((int)(simulation.clockInMinutes()) >= 1
                     && count == 0){
                 createFaultInjectionForHosts(datacenter);
                 count++;
@@ -150,23 +243,58 @@ public class Client {
             hostAllocationReply.put("desc", "host_allocation");
             hostAllocationReply.put("client_id", vmHostEventInfo.getVm().getDescription());
 
-            int find = unchangedList.size() - 1;
+            int find = -1;
+            for(int i = 0; i < unchangedList.size(); i++){
+                if(unchangedList.get(i) == vmHostEventInfo.getVm()){
+                    find = i;
+                    break;
+                }
+            }
             int vmID = -1;
             Set<Map.Entry<Integer, Integer>> entries = idMap.entrySet();
             for(Map.Entry<Integer, Integer> entry : entries){
                 if(entry.getValue() == find){
                     vmID = entry.getKey();
+                    break;
                 }
             }
 
             hostAllocationReply.put("vm_id", vmID + "");
 
+            //System.out.println(hostAllocationReply);
             try {
                 currSession.getBasicRemote().sendObject(hostAllocationReply);
             } catch (IOException | EncodeException e) {
                 e.printStackTrace();
             }
         };
+
+        File file = new File(args[0]);
+        String arguments = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String st;
+            while ((st = br.readLine()) != null) {
+                arguments = st;
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        String[] argsArray = arguments.split(" ");
+        for(String str : argsArray){
+            System.out.print(str + " ");
+        }
+        betaDistributionLocation.reseedRandomGenerator(Long.parseLong(argsArray[2]));
+        betaDistributionHost.reseedRandomGenerator(Long.parseLong(argsArray[3]));
+        betaDistributionIndex.reseedRandomGenerator(Long.parseLong(argsArray[4]));
+
+        for(int i = 0; i < TOTAL_LOCATIONS; i++) {
+            locationThreshold.put(i, new Pair(Double.parseDouble(argsArray[0]), Double.parseDouble(argsArray[1])));
+        }
+
+        locationThresholdEntrySet = locationThreshold.entrySet();
 
         ClientManager client = ClientManager.createClient();
         try {
@@ -241,10 +369,9 @@ public class Client {
             int id = (int)(vm.getId());
             int startIdx = factor * id;
             int min = Integer.MAX_VALUE;
-            List<Host> list = vmAllocationPolicy.getHostList();
             Host rv = null;
             for(int i = startIdx; i < startIdx + factor; i++){
-                Host host = list.get(i);
+                Host host = hostList.get(i);
                 if(min > host.getVmList().size()){
                     min = host.getVmList().size();
                     rv = host;
@@ -270,12 +397,12 @@ public class Client {
         List<Pe> peList = new ArrayList<>(HOST_PES);
 
         for (int i = 0; i < HOST_PES; i++) {
-            peList.add(new PeSimple(10000, new PeProvisionerSimple()));
+            peList.add(new PeSimple(20000, new PeProvisionerSimple()));
         }
 
-        final long ram = 16384; // Random number as of now, TODO: Get this initially from FTM through a socket connection probably. DONE 16GB.
-        final long bw = 40960; // Random number as of now, TODO: Get this initially from FTM through a socket connection probably. DONE 40Mbps.
-        final long storage = 51200; // Random number as of now, TODO: Get this initially from FTM through a socket connection probably. DONE 50GB.
+        final long ram = 131072; // Random number as of now, TODO: Get this initially from FTM through a socket connection probably. DONE 128GB.
+        final long bw = 327680; // Random number as of now, TODO: Get this initially from FTM through a socket connection probably. DONE 40 * 8Mbps.
+        final long storage = 819200; // Random number as of now, TODO: Get this initially from FTM through a socket connection probably. DONE 800GB.
         Host host = new HostSimple(ram, bw, storage, peList);
         host
                 .setRamProvisioner(new ResourceProvisionerSimple())
@@ -305,6 +432,7 @@ public class Client {
             JSONObject vmConfig = vmArray.getJSONObject(i);
             String clientID = "";
             if(message.has("client_id")){
+                finishClientID = message.getString("client_id");
                 clientID = message.getString("client_id");
             }
             int vmID = Integer.parseInt(vmConfig.getString("vm_id"));
@@ -587,9 +715,21 @@ public class Client {
             clientID = message.getString("client_id");
         }
 
+        JSONObject migrationStarted = new JSONObject();
+        migrationStarted.put("desc", "migration_successful");
+        migrationStarted.put("client_id", clientID);
+        migrationStarted.put("vm_id", vmID);
+
         Vm vm = unchangedList.get(mappedID);
         for(Host host : hostList){
-            if(host.getVmList().isEmpty() && host.isSuitableForVm(vm)){
+            if(host.getVmList().isEmpty() && host.isSuitableForVm(vm) && host.getVmsMigratingIn().isEmpty()){
+                String delay = vm.getRam().getCapacity() / Conversion.bitesToBytes(host.getBw().getCapacity() * datacenter.getBandwidthPercentForMigration()) + "";
+                migrationStarted.put("delay", delay);
+                try {
+                    currSession.getBasicRemote().sendObject(migrationStarted);
+                } catch (IOException | EncodeException e) {
+                    e.printStackTrace();
+                }
                 datacenter.requestVmMigration(vm, host);
                 return;
             }
@@ -599,7 +739,7 @@ public class Client {
         Host vmHost = unchangedList.get(mappedID).getHost();
         Host temp = null;
         for(Host host : hostList){
-            if(min >= host.getVmList().size()){
+            if(min > host.getVmList().size()){
                 if(host != vmHost){
                     min = host.getVmList().size();
                     temp = host;
@@ -607,8 +747,16 @@ public class Client {
             }
         }
 
-        if(temp == null){
+        /*if(temp == null){
             return;
+        }*/
+
+        String delay = vm.getRam().getCapacity() / Conversion.bitesToBytes(temp.getBw().getCapacity() * datacenter.getBandwidthPercentForMigration()) + "";
+        migrationStarted.put("delay", delay);
+        try {
+            currSession.getBasicRemote().sendObject(migrationStarted);
+        } catch (IOException | EncodeException e) {
+            e.printStackTrace();
         }
 
         datacenter.requestVmMigration(vm, temp);
@@ -764,7 +912,7 @@ public class Client {
             JSONObject rv = statusVm(message);
             try {
                 currSession.getBasicRemote().sendObject(rv);
-                System.out.println(rv);
+                //System.out.println(rv);
             } catch (IOException | EncodeException e) {
                 e.printStackTrace();
             }
