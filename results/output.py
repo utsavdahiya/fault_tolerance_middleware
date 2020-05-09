84a3e131443866127461ce9c38ebf8354ab6e5ca
utsav <utsav.163dahiya@gmail.com>
2020-05-09 19:40:26 +0530

    cloud error logs

--- results/output.py
+++ results/output.py
@@ -185,8 +185,8 @@ def print_comparision(file1, file2):
 
 
 def main():
-    file1 = "OutputUday18.pkl"   #new
-    file2 = "OutputUday19.pkl"   #old
+    file1 = "OutputUday20.pkl"   #new
+    file2 = "OutputUday21.pkl"   #old
 
     # with open(file1, 'rb') as handle:
     #     result = pickle.load(handle)
