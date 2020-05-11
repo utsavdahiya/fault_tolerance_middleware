8288237e079da07227a5ea2691bcfefd08e0c61d
utsav <utsav.163dahiya@gmail.com>
2020-05-11 12:20:39 +0530

    seed_testing

--- seed_test/seed_testing.py
+++ seed_test/seed_testing.py
@@ -5,22 +5,31 @@ import pickle
 
 START_SEED = 0
 NUM_LOCATIONS = 4
-NUM_VM_SET = 5
+NUM_VM_SET = 500
 SET_SIZE = 3
-NUM_ITERATIONS = 10
+NUM_ITERATIONS = 100
 OUTPUT_FILE = './default_output.pkl'
 
-def place_vm(seed_result, loc_result, togetherness_result):
+def place_vm():
+    total_result = []
     location_array = [i for i in range(NUM_LOCATIONS)]
     seed = START_SEED
     for i in range(NUM_ITERATIONS):
         print("Iter start------------------")
         random.seed(seed)
 
+        seed_result = {
+            'seed': seed,
+            'score': 0,
+            'togetherness': {},
+            'placement': [0] * NUM_LOCATIONS
+        }
+
         #now place the sets
         for Set in range(NUM_VM_SET):
             togetherness = {}
-            placement = [0] * NUM_LOCATIONS 
+            placement = [0] * NUM_LOCATIONS
+            
             for vm in range(SET_SIZE):
                 loc = random.choice(location_array)
                 placement[loc] += 1
@@ -39,45 +48,32 @@ def place_vm(seed_result, loc_result, togetherness_result):
                     else:
                         togetherness[num] = 1
 
-            #calculating score for this
-            score = 0
-            for key, val in togetherness.items():
-                score += key * val  #a weighted sum of the togetherness
-
             #updating the result
-            loc_result = [sum(i) for i in zip(loc_result, placement)]
-            togetherness_result = Counter(togetherness_result) + Counter(togetherness)
-            seed_val = {'seed': seed,
-                        'score':score,
-                        'togetherness': togetherness,
-                        'placement': placement}
+            seed_result['placement'] = [sum(i) for i in zip(seed_result['placement'], placement)]
+            seed_result['togetherness'] = Counter(seed_result['togetherness']) + Counter(togetherness)
 
-            seed_result.append(seed_val)
+        #calculating score for this
+        score = 0
+        for key, val in seed_result['togetherness'].items():
+            score += key * val  #a weighted sum of the togetherness
+        seed_result['score'] = score
+        total_result.append(seed_result)
         
         seed += 1
-    return seed, loc_result, togetherness_result
+    return seed, total_result
 
 def main():
-    seed_result = []
-    loc_result = [0] * NUM_LOCATIONS
-    togetherness_result = {}
-    for i in range(SET_SIZE+1):
-        if i==1:
-            continue
-        else:
-            togetherness_result[i] = 0
+    total_result = []
 
-    end_seed, loc_result, togetherness_result = place_vm(seed_result, loc_result, togetherness_result)
+    end_seed, total_result = place_vm()
     print(f"ENDING at seed: {end_seed}")
 
-    sorted(seed_result, reverse=True, key=lambda seed: seed['score'])
-    store = {'seed_result':seed_result,
-               'loc_result': loc_result,
-               'togetherness_result': togetherness_result}
-    print(f"store: \n{store}")
+    sorted(total_result, reverse=True, key=lambda seed: seed['score'])
+    # store = {'seed_result':total_result}
+    print(f"store: \n{total_result}")
     #writing the results to output file
     with open(OUTPUT_FILE, '+wb') as handle:
         print("file opened")
-        store = pickle.dump(store, handle)
+        store = pickle.dump(total_result, handle)
 
 main()
