43d8385da2b40194002edca5a69ad225f0aa9b09
utsav <utsav.163dahiya@gmail.com>
2020-05-12 00:17:29 +0530

    VMs-> 170*3

--- results/output.py
+++ results/output.py
@@ -185,8 +185,8 @@ def print_comparision(file1, file2):
 
 
 def main():
-    file1 = "OutputUday27.pkl"   #new
-    file2 = "OutputUday28.pkl"   #old
+    file1 = "output-16-vaibhav.pkl"   #new
+    file2 = "output-17-vaibhav.pkl"   #old
 
     # with open(file1, 'rb') as handle:
     #     result = pickle.load(handle)
